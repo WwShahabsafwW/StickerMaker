7a3ec73e52a2bb2a3d1f3da7dd2fec95096af5f2
Shahab <safaeishahab@yahoo.com>
2016-03-15 13:14:53 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,91 +1,19 @@
 local function run(msg, matches)
-  if is_chat_msg(msg) then
-    local text = [[‌✅Commands to lock|unlock
-💭/close|open link
-💭/close|open member
-💭/close|open name
-💭/close|open bot
-💭/close|open photo
-💭/close|open sticker
-💭/close|open file 
-💭/close|open audio
+  if is_user_msg(msg) then
+    local text = [[‌✅ How To Use ✅
 
-➕
-✅Commands for control member
-💭/kick : by <reply|id|username>
-💭/ban : by <reply|id|username>
-💭/unban : by <reply|id|username>
-💭/kickme
+Send me photo first !
+Then send by pm "Sticker"
+I will give you your sticker
 
-➕
-✅Group control 
-💭/rules
-💭/setrules <write rules>
-💭/about
-💭/setabout <write about>
-💭/setphoto : then send photo
-💭/setname <write name>
-💭/id
-💭/id chat
-💭/group settings 
-💭/getlink : send link in your pv
-💭/relink <idchat> : change link group and send new link your pv
-💭/modlist
-💭/help
-
-➕
-✅ Group Promote  commands
-💭/spromote : up leader by <reply|id|username> 
-💭/sdemote : in promote by <reply|id|username>
-💭/promote : by <reply|id|username> 
-💭/demote : by <reply|id|username> 
+Developer : @Shahabambesik
+E M C Team™
 
 ➖🔸➖🔹➖🔸➖🔹➖]]
     return text
   end
-  if is_channel_msg(msg) then
-    local text = [[‌‌✅Commands to lock|unlock
-💭/close|open link
-💭/close|open member
-💭/close|open name
-💭/close|open bot
-💭/close|open photo
-💭/close|open sticker
-💭/close|open file 
-💭/close|open audio
-💭/close|open talk
-
-➕
-✅Commands for control member
-💭/kick : by <reply|id|username>
-💭/ban : by <reply|id|username>
-💭/unban : by <reply|id|username>
-💭/kickme
-
-➕
-✅Group control 
-💭/rules
-💭/setrules <write rules>
-💭/about
-💭/setabout <write about>
-💭/setphoto : then send photo
-💭/setname <write name>
-💭/id
-💭/id chat
-💭/group settings 
-💭/getlink : send link in your pv
-💭/relink <idchat> : change link group and send new link your pv
-💭/modlist
-💭/help
-
-➕
-✅ Group Promote  commands
-💭/spromote : up leader by <reply|id|username> 
-💭/sdemote : in promote by <reply|id|username>
-💭/promote : by <reply|id|username> 
-💭/demote : by <reply|id|username> 
-
-➖🔸➖🔹➖🔸➖🔹➖]]
+  --if is_channel_msg(msg) then
+    --local text = [[‌‌]]
     return text
   else
     local text = [[aaa]]
